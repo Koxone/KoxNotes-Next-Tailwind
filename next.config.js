19d70f302c58ceaaea9166f8624f22a1f7099395
Carlos de Leon <admin@koxland.net>
2025-05-21 21:06:22 -0600

    x

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: false
-  }
+  },
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
