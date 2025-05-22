7b020be2cf1abb55ae70a7e9bf4ef204832ac456
Carlos de Leon <admin@koxland.net>
2025-05-21 21:13:42 -0600

    d

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: false
-  },
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true
 };
-
-module.exports = nextConfig;
